9ee7f873910ae8adf37e87eeeae2dca58dac206c
andricevics <stjepan.andricevic@less.hr>
2023-03-29 07:29:33 +0200

    Added project to repository

---
+++ src/test/java/com/hr/fer/rsikspr/FirstAssignment/FirstAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.fer.rsikspr.FirstAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
